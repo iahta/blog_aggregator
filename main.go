6c964d1ebedbe54ab377a946ea1949f0bd03e7f3
iahta <153889016+iahta@users.noreply.github.com>
2025-03-30 21:59:38 -0500

    need to register

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/iahta/blog_aggregator/internal/config"
+	"github.com/iahta/blog_aggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
+// give access to handlers to the application state, state struct holds pointer to a config
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,20 +22,30 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error reading database: %v", err)
+	}
+	dbQueries := database.New(db)
+
+	//storing the config in a new instance of the state struct
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
-
+	//new instance of the commands struct with an initialized map of handler functions
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+	//register the handler with its function
 	cmds.register("login", handlerLogin)
 
+	//os.Args used to get the cl arguments passed in
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
 		return
 	}
-
+	//split the arguments to create a command instance
 	cmdName := os.Args[1]
 	cmdArgs := os.Args[2:]
 
